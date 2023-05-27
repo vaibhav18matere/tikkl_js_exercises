1b14ebb359f331338f4fc9df59e57a04f62ea59f
Vaibhav Matere <vpm00718897@gmail.com>
2023-05-27 11:44:01 +0530

    assignment 1 completed

--- ex1.js
+++ ex1.js
@@ -3,27 +3,23 @@
 // Run this on the terminal window
 //  $ node ex1.js
 
-function build_user(name, age, occupation){
-  obj = {} // creates a new JS object
+function build_user(name, age, occupation) {
+  obj = {}; // creates a new JS object
 
   // create properties dynamically
-  obj.name = name
-  obj.age = age
-  obj.occupation = occupation
-  
+  obj.name = name;
+  obj.age = age;
+  obj.occupation = occupation;
+
   // returns an initialized user object.
-  return obj
+  return obj;
 }
 
-u1 = build_user("Mary Jane Tyler", 24, "Accountant")
-u2 = build_user("Steve King", 22, "Dentist")
-u3 = build_user("Preet Minari", 25, "Attorney")
+u1 = build_user("Mary Jane Tyler", 24, "Accountant");
+u2 = build_user("Steve King", 22, "Dentist");
+u3 = build_user("Preet Minari", 25, "Attorney");
 
-console.log(u1)
-console.log(u2)
-console.log(u3)
-
-// Note: The program continues below. 
+// Note: The program continues below.
 // Each of the excerises should continue updating the object.
 
 // Ex 1.1. Change the name of u1 to "Mary-Jane Tyson" (use plain property assignment)
@@ -31,3 +27,28 @@ console.log(u3)
 // Ex 1.3. Use string templates using `` and ${} to prefix occupation of u3 with "District "
 // Ex 1.4. Make an array of three user objects. (use the form -- a = [x, y, ...])
 // Ex 1.5. Loop through the array and log each object to the console.
+
+// solution 1.1
+u1.name = "Mary Jane Tyson";
+console.log(u1);
+
+// solution 1.2
+u2.age = u2.age += 5;
+console.log(u2);
+
+// solution 1.3
+u3 = build_user("Preet Minari", 25, "Attorney");
+u3.occupation = `District ${u3.occupation}`;
+console.log(u3);
+
+// solution 1.4
+const users = [
+  build_user("Mary Jane Tyson", 24, "Accountant"),
+  build_user("Steve King", 22, "Dentist"),
+  build_user("Preet Minari", 25, "Attorney"),
+];
+
+// solution 1.5
+users.forEach((user) => {
+  console.log("user", user);
+});
