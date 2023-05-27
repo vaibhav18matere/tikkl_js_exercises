04455745cbc07e73d487889c55e2b2cde1e50eb7
Vaibhav Matere <vpm00718897@gmail.com>
2023-05-27 11:27:13 +0530

    feat : ex 2 completed

--- ex2.js
+++ ex2.js
@@ -1,41 +1,43 @@
 // Functions are first-class objects in JS.
 // -- you can pass them around like any other data.
 
-function double(x){
-  return x * 2
+function double(x) {
+  return x * 2;
 }
 
 // assign a function to a variable.
 const square = function (x) {
-  return x * x
-}
+  return x * x;
+};
 
 // define a method using the arrow operator.
 // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 const cube = (x) => square(x) * x;
 
 // method takes another method as argument.
-function applyMethod(method, val){
-  return method(val)
+function applyMethod(method, val) {
+  return method(val);
 }
 
 // Pass a method as an arg
-let v = applyMethod(double, 10)
-console.log('double: ', v)
+let v = applyMethod(double, 10);
+console.log("double: ", v);
 
 // Method from a variable is just a method.
-v = applyMethod(square, 10)
-console.log('square: ', v)
+v = applyMethod(square, 10);
+console.log("square: ", v);
 
 // Methods can be defined on the fly with no name -- lambda
-v = applyMethod((x) => x * 3, 10)
-console.log('x3: ', v)
+v = applyMethod((x) => x * 3, 10);
+console.log("x3: ", v);
 
-const numbers = [7, 4, 3, 12, 9]
-console.log('doubles: ')
+const numbers = [7, 4, 3, 12, 9];
+console.log("doubles: ");
 
 // The forEach member function of Array takes a method and runs it for each element.
-numbers.forEach((n) => console.log(applyMethod(double, n)))
+numbers.forEach((n) => console.log(applyMethod(double, n)));
 
 // Ex 2.1 Write code to print out value/2 of each number in the numbers array.
 // - Use forEach and a lambda function.
+
+numbers.forEach((n) => console.log(applyMethod((x) => x / 2, n)));
