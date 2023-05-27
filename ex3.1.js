5fb4e673c85f5dc3e143a82bdb461fd91786bb60
Vaibhav Matere <vpm00718897@gmail.com>
2023-05-27 11:50:08 +0530

    3.1 completed

--- ex3.1.js
+++ ex3.1.js
@@ -23,20 +23,30 @@ The implementation of readChunk would be something like this,
 generates a random number [0,100) with a delay of 200ms.
 See: https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
 */
-function getData(callback){
-  setTimeout(
-    () => callback(Math.round(Math.random() * 100)), 
-    200
-  )
+function getData(callback) {
+  setTimeout(() => callback(Math.round(Math.random() * 100)), 200);
 }
 
-console.log('Callback Implementation');
+console.log("Callback Implementation");
 
 // Ex 3.1. Rewrite the code below using arrow function `(x) => code`
-getData(function(n){
-  console.log('Got some data:', n);
-})
+getData(function (n) {
+  console.log("Got some data from modified function:", n);
+});
+
+// Solution 3.1
+const getData2 = (n) => {
+  console.log("Got some data:", n);
+};
 
 // Ex 3.2. Write a loop that prints out 10 items of data from getData.
 
-console.log('will be processing data soon...');
+// solution 3.2
+
+for (let i = 0; i < 10; i++) {
+  getData((data) => {
+    console.log(data);
+  });
+}
+
+console.log("will be processing data soon...");
